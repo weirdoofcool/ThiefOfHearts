3a7e1bcf6894d30a6a4b1375ba65f0db4f03c475
weirdoofcool <59780020+weirdoofcool@users.noreply.github.com>
2021-09-26 19:34:05 -0400

    Add files via upload

---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public float Speed;
+    Rigidbody rigidbody3D;
+    public bool isOnGround = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody3D = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(hor, 0f, ver) * Speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+    }
+
+    void FixedUpdate()
+    {
+        if(Input.GetButtonDown("Jump") && isOnGround == true)
+        {
+            rigidbody3D.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Ground")
+        {
+            isOnGround = true;
+        }
+    }
+}
